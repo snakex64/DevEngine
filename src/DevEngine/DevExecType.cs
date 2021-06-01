7dfc8d1cae62443bddde92825a5f2c5ccffa9c10
pascx64 <pascx64@gmail.com>
2021-05-31 22:05:42 -0400

    Added DevExecType

---
+++ src/DevEngine/DevExecType.cs
@@ -0,0 +1,21 @@
+﻿using DevEngine.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevEngine
+{
+    public class DevExecType : IDevType
+    {
+        public string TypeName => "Exec";
+
+        public string TypeNamespace => "System";
+
+        public bool IsClass => false;
+
+        public bool CanBeAssignedTo(IDevType type)
+        {
+            return type is DevExecType;
+        }
+    }
+}
