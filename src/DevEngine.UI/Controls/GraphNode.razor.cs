3cae478f3ae947f733049ffe7f259bb7bcd3e0a9
pascx64 <pascx64@gmail.com>
2021-06-21 20:38:27 -0400

    Added basic node dragging, setup basic node css styling

---
+++ src/DevEngine.UI/Controls/GraphNode.razor.cs
@@ -0,0 +1,71 @@
+﻿using DevEngine.UI.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevEngine.UI.Controls
+{
+    public partial class GraphNode : ComponentBase
+    {
+        [Parameter]
+        public Core.Graph.IDevGraphDefinition DevGraphDefinition { get; set; }
+
+        [Parameter]
+        public Core.Graph.IDevGraphNode DevGraphNode { get; set; }
+
+        private GraphNodeSavedContent? GraphNodeSavedContent { get; set; }
+
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+
+            if (firstRender)
+            {
+                if (DevGraphNode.AdditionalContent.TryGetValue("GraphNodeUI", out var content))
+                    GraphNodeSavedContent = System.Text.Json.JsonSerializer.Deserialize<GraphNodeSavedContent>(content);
+                else
+                    GraphNodeSavedContent = new GraphNodeSavedContent();
+
+                DevGraphNode.AdditionalContentProvider["GraphNodeUI"] = () => System.Text.Json.JsonSerializer.Serialize(GraphNodeSavedContent);
+
+                StateHasChanged();
+            }
+        }
+
+        #region Dragging
+
+        private double DragStartX;
+        private double DragStartY;
+
+        System.Drawing.PointF InitialDragNodeLocation;
+
+        private void OnDragStart(DragEventArgs args)
+        {
+            if (GraphNodeSavedContent == null)
+                throw new Exception("GraphNodeSavedContent should be set");
+
+            DragStartX = args.ClientX;
+            DragStartY = args.ClientY;
+
+            InitialDragNodeLocation = GraphNodeSavedContent.Location;
+        }
+
+        private void OnDrag(DragEventArgs args)
+        {
+            if (GraphNodeSavedContent == null)
+                throw new Exception("GraphNodeSavedContent should be set");
+
+            GraphNodeSavedContent.Location = new System.Drawing.PointF((float)(InitialDragNodeLocation.X + args.ClientX - DragStartX), (float)(InitialDragNodeLocation.Y + args.ClientY - DragStartY));
+        }
+        private void OnDragEnd(DragEventArgs args)
+        {
+            OnDrag(args);
+        }
+
+        #endregion
+    }
+}
